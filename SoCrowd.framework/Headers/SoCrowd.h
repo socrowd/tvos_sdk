d785ab7a9bf92b5d45433b875abf8c97636d5bed
Aurel <aurel@socrowd.tv>
2016-02-18 13:43:00 +0100

    Initial test distribution

---
+++ SoCrowd.framework/Headers/SoCrowd.h
@@ -0,0 +1,19 @@
+//
+//  SoCrowd SDK
+//
+//  Created by Aurel on 10/02/16.
+//  Copyright © 2016 SoCrowd.tv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sosdk.
+FOUNDATION_EXPORT double sosdkVersionNumber;
+
+//! Project version string for sosdk.
+FOUNDATION_EXPORT const unsigned char sosdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sosdk/PublicHeader.h>
+
+
+#import "XMLDictionary.h"
